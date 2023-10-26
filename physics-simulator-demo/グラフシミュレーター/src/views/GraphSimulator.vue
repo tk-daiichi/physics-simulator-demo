73e5b3710d6b8fdc576d86de6f8890c27d28bdde
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-10-26 16:48:06 +0900

    canvasのサイズがwindowサイズに合わせて更新されるよう設定

--- physics-simulator-demo/グラフシミュレーター/src/views/GraphSimulator.vue
+++ physics-simulator-demo/グラフシミュレーター/src/views/GraphSimulator.vue
@@ -1,115 +1,124 @@
 <template>
-    <h1>グラフシミュレータ</h1>
-
-    <v-container class="">
-
-    <div id="graphInfo">
-        <div id="graph1">
-            <h2 class="mb-2">y=ax+bのグラフ</h2>
-            <div id="paramA">
-                <label id="param">a</label>
-                <v-btn id="parambtn" @click="() => {graph1a -= 1, paramUpdate()}">&lt;</v-btn>
+    <div class="sim-body">
+        <h1>グラフシミュレータ</h1>
+        <!-- <div>width:{{ windowWidth }}, origin:{{ origin }}, scale:{{ scaleX }}, {{ scaleY }}</div> -->
+        <canvas 
+            id="ca" ref="canvasRef"
+            class=""
+            :width=scaleX :height=scaleY 
+            @mousedown = "mouseStatus = 'mousedown'"
+            @mouseup   = "mouseStatus = 'mouseup'"
+            @mousemove="scrollGraph">
+        </canvas>
+    
+        <div id="graphInfo">
+            <div id="graph1">
+                <h2 class="mb-2">y=ax+bのグラフ</h2>
+                <div id="paramA">
+                    <label id="param">a</label>
+                    <v-btn id="parambtn" @click="() => {graph1a -= 1, paramUpdate()}">&lt;</v-btn>
+                    <input 
+                        for="param" type="number" 
+                        id="paramInput"
+                        v-model="graph1a" 
+                        @keydown.enter="paramUpdate"
+                        @change="paramUpdate">
+                    <v-btn id="parambtn" @click="() => {graph1a += 1, paramUpdate()}">&gt;</v-btn>
+                </div>
+                <div id="paramB">
+                    <label id="param">b</label>
+                    <v-btn id="parambtn" @click="() => {graph1b -= 1, paramUpdate()}">&lt;</v-btn>
+                    <input
+                        for="param" type="number"
+                        id="paramInput"
+                        v-model="graph1b" 
+                        @keydown.enter="paramUpdate"
+                        @change="paramUpdate">
+                    <v-btn id="parambtn" @click="() => {graph1b += 1, paramUpdate()}">&gt;</v-btn>
+                </div>
+                <div id="graphEq">
+                    グラフの式：{{ graph1Eq(graph1a, graph1b) }}
+                </div>
+            </div>
+            
+            <div id="graph2">
+                <h2>y=a(x-b)<sup>2</sup>+cのグラフ</h2>
+                <div id="paramA">
+                    <label id="param">a</label>
+                    <v-btn id="parambtn" @click="() => {graph2a -= 1, paramUpdate()}">&lt;</v-btn>
+                    <input
+                        for="param" type="number" 
+                        id="paramInput"
+                        v-model="graph2a" 
+                        @keydown.enter="paramUpdate"
+                        @change="paramUpdate">
+                    <v-btn id="parambtn" @click="() => {graph2a += 1, paramUpdate()}">&gt;</v-btn>
+                </div>
+                <div id="paramB">
+                    <label id="param">b</label>
+                    <v-btn id="parambtn" @click="() => {graph2b -= 1, paramUpdate()}">&lt;</v-btn>
+                    <input
+                        for="param" type="number" 
+                        id="paramInput"
+                        v-model="graph2b" 
+                        @keydown.enter="paramUpdate"
+                        @change="paramUpdate">
+                    <v-btn id="parambtn" @click="() => {graph2b += 1, paramUpdate()}">&gt;</v-btn>
+                </div>
+                <div id="paramC">
+                    <label id="param">c</label>
+                    <v-btn id="parambtn" @click="() => {graph2c -= 1, paramUpdate()}">&lt;</v-btn>
+                    <input
+                        for="param" type="number" 
+                        id="paramInput"
+                        v-model="graph2c" 
+                        @keydown.enter="paramUpdate"
+                        @change="paramUpdate">
+                    <v-btn id="parambtn" @click="() => {graph2c += 1, paramUpdate()}">&gt;</v-btn>
+                </div>
+                <div id="graphEq">
+                    グラフの式：<span v-html="graph2Eq(graph2a, graph2b, graph2c)"></span>
+                </div>
+            </div>
+    
+            <div>
+                <label id="intervalSlider">縮尺</label>
                 <input 
-                    for="param" type="number" 
-                    id="paramInput"
-                    v-model="graph1a" 
-                    @keydown.enter="paramUpdate"
-                    @change="paramUpdate">
-                <v-btn id="parambtn" @click="() => {graph1a += 1, paramUpdate()}">&gt;</v-btn>
+                    for="intervalSlider" type="range"
+                    min="5" max="200"
+                    v-model="interval"
+                    @input="paramUpdate()">
             </div>
-            <div id="paramB">
-                <label id="param">b</label>
-                <v-btn id="parambtn" @click="() => {graph1b -= 1, paramUpdate()}">&lt;</v-btn>
-                <input
-                    for="param" type="number"
-                    id="paramInput"
-                    v-model="graph1b" 
-                    @keydown.enter="paramUpdate"
-                    @change="paramUpdate">
-                <v-btn id="parambtn" @click="() => {graph1b += 1, paramUpdate()}">&gt;</v-btn>
-            </div>
-            <div id="graphEq">
-                グラフの式：{{ graph1Eq(graph1a, graph1b) }}
-            </div>
-        </div>
-        
-        <div id="graph2">
-            <h2>y=a(x-b)<sup>2</sup>+cのグラフ</h2>
-            <div id="paramA">
-                <label id="param">a</label>
-                <v-btn id="parambtn" @click="() => {graph2a -= 1, paramUpdate()}">&lt;</v-btn>
-                <input
-                    for="param" type="number" 
-                    id="paramInput"
-                    v-model="graph2a" 
-                    @keydown.enter="paramUpdate"
-                    @change="paramUpdate">
-                <v-btn id="parambtn" @click="() => {graph2a += 1, paramUpdate()}">&gt;</v-btn>
-            </div>
-            <div id="paramB">
-                <label id="param">b</label>
-                <v-btn id="parambtn" @click="() => {graph2b -= 1, paramUpdate()}">&lt;</v-btn>
-                <input
-                    for="param" type="number" 
-                    id="paramInput"
-                    v-model="graph2b" 
-                    @keydown.enter="paramUpdate"
-                    @change="paramUpdate">
-                <v-btn id="parambtn" @click="() => {graph2b += 1, paramUpdate()}">&gt;</v-btn>
-            </div>
-            <div id="paramC">
-                <label id="param">c</label>
-                <v-btn id="parambtn" @click="() => {graph2c -= 1, paramUpdate()}">&lt;</v-btn>
-                <input
-                    for="param" type="number" 
-                    id="paramInput"
-                    v-model="graph2c" 
-                    @keydown.enter="paramUpdate"
-                    @change="paramUpdate">
-                <v-btn id="parambtn" @click="() => {graph2c += 1, paramUpdate()}">&gt;</v-btn>
-            </div>
-            <div id="graphEq">
-                グラフの式：<span v-html="graph2Eq(graph2a, graph2b, graph2c)"></span>
-            </div>
-        </div>
-        <div>
-            <label id="intervalSlider">縮尺</label>
-            <input 
-                for="intervalSlider" type="range"
-                min="50" max="200"
-                v-model="interval"
-                @input="paramUpdate()"
-            >
         </div>
     </div>
-
-    <canvas 
-        id="ca" ref="canvasRef" 
-        :width=scaleX :height=scaleY 
-        @mousedown = "mouseStatus = 'mousedown'"
-        @mouseup   = "mouseStatus = 'mouseup'"
-        @mousemove="scrollGraph"
-        >
-    </canvas>
-
-</v-container>
 </template>
 
 <script setup lang="ts">
-    import { ref, onMounted } from 'vue';
+    import { ref, onMounted, reactive } from 'vue';
     import { coordinate, Origin } from '@/components/CoordinateDrawer'
     import { clearGraph, graph1Draw, graph2Draw } from '@/components/GraphDrawer'
     import { graph1Eq, graph2Eq } from '@/components/GraphEquality'
 
     const canvasRef = ref<HTMLCanvasElement>()
 
-    let origin: Origin = {x: 500, y: 500};
-
-    const scaleX = 1000;
-    const scaleY = 600;
-
-    const interval = ref<number>(100);
-
+    const windowWidth   = ref<number>(window.innerWidth)
+    const scaleX        = ref<number>(windowWidth.value * 0.8);
+    const scaleY        = ref<number>(windowWidth.value * 0.7);
+    let origin: Origin  = reactive({x: scaleX.value*0.5, y: scaleY.value*0.5});
+    
+    window.onresize = (() => {
+        new Promise ((resolve) => {
+            windowWidth.value   = window.innerWidth
+            scaleX.value        = windowWidth.value * 0.8
+            scaleY.value        = windowWidth.value * 0.7
+            origin              = {x: windowWidth.value * 0.4, y: windowWidth.value * 0.4}
+            resolve("");
+        }).then(() => {
+            paramUpdate()
+        })
+    })
+    const interval = 100;
+    
     const graph1a = ref<number>(1);
     const graph1b = ref<number>(-2);
     const graph2a = ref<number>(-1)
@@ -119,9 +128,9 @@
     const paramUpdate = () => {
         const ctx = canvasRef.value?.getContext("2d")
         if(ctx){
-            clearGraph(ctx, scaleX, scaleY, interval.value, origin);
-            graph1Draw(ctx, scaleX, scaleY, interval.value, origin, graph1a.value, graph1b.value);
-            graph2Draw(ctx, scaleX, scaleY, interval.value, origin, graph2a.value, graph2b.value, graph2c.value);
+            clearGraph(ctx, scaleX.value, scaleY.value, interval, origin);
+            graph1Draw(ctx, scaleX.value, scaleY.value, interval, origin, graph1a.value, graph1b.value);
+            graph2Draw(ctx, scaleX.value, scaleY.value, interval, origin, graph2a.value, graph2b.value, graph2c.value);
         }
     }
 
@@ -148,6 +157,9 @@
 </script>
 
 <style>
+    .sim-body {
+        text-align: left;
+    }
     #graphInfo {
         display: flex;
         justify-content: space-around;
