e717a90b0de7bb21f0fb9936e31029e17e28b93e
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-01 17:36:25 +0900

    水平投射とその軌跡を追加

--- physics-simulator-demo/BallLauncher/src/views/BallLauncher.vue
+++ physics-simulator-demo/BallLauncher/src/views/BallLauncher.vue
@@ -11,11 +11,11 @@
             </v-layer>
             <v-layer :config="layer_cfg" ref="ball_layer">
                 <v-circle 
-                    :config="circle_cfg" ref="circle2">
+                    :config="circle_cfg2" ref="circle2">
                 </v-circle>
-                <v-circle 
+                <!-- <v-circle 
                     :config="circle_cfg" ref="circle">
-                </v-circle>
+                </v-circle> -->
             </v-layer>
         </v-stage>
     </div>
@@ -31,62 +31,89 @@
     const stage = ref<Konva.Stage>()
     const cor_layer = ref<Konva.Layer>()
     const ball_layer = ref<Konva.Layer>()
-    const circle = ref<Konva.Circle>()
+    // const circle = ref<Konva.Circle>()
+    const circle2 = ref<Konva.Circle>()
 
     const posX = ref<number>()
     const posY = ref<number>()
     onMounted(() => {
-        if(circle.value){
-            bounceball(circle.value, stage_cfg, circle_cfg)
-        }
-    })
-    function bounceball(
-        shape: Konva.Circle, 
-        stage_cfg: StageConfig,
-        shape_cfg: CircleConfig,
-    ){
-        // @ts-ignore
-        const circleNode = shape.getNode()
-        let positionX = shape_cfg.x                    
-        let positionY = shape_cfg.y
-
-        const period: number = 3;
-        let velX = 10 / period;
-        let velY = velX;
-
-        let anim = new Konva.Animation(function(frame) {
-            if(frame){
-                let borderXl = positionX - shape_cfg.radius + velX;
-                let borderXr = positionX + shape_cfg.radius + velX;
-                if(borderXl > 0 && borderXr < stage_cfg.width){
-                } else if(borderXl <= 0){
-                    velX = frame.timeDiff / period;
-                } else if (borderXr >= stage_cfg.width) {
-                    velX = -frame.timeDiff / period;
-                };
-                
-                let borderYt = positionY - shape_cfg.radius + velY;
-                let borderYb = positionY + shape_cfg.radius + velY;
-                if(borderYt > 0 && borderYb < stage_cfg.height){
-                } else if(borderYt <= 0){
-                    velY = frame.timeDiff / period;
-                } else if (borderYb >= stage_cfg.height) {
-                    velY = -frame.timeDiff / period;
-                };
-
-                positionX += velX;
-                positionY += velY;
-                circleNode.setX(positionX);
-                circleNode.setY(positionY);
-
-                if (positionX && positionY){
-                    posX.value = Math.round(positionX);
-                    posY.value = Math.round(positionY);
+        if(circle2.value){
+            //@ts-ignore
+            const circle2node = circle2.value.getNode();
+            let period = 200;
+            
+            
+            let anim = new Konva.Animation(function(frame){
+                if(frame) {
+                    let aaa = frame.time / period;
+                    let velX = 50;
+                    circle2node.setX(circle_cfg2.x + velX * aaa)
+                    circle2node.setY(circle_cfg2.y +  9.8 * aaa ** 2)
+                    let circle = new Konva.Circle({
+                        x: circle_cfg2.x + velX * aaa,
+                        y: circle_cfg2.y +  9.8 * aaa ** 2,
+                        radius: 10,
+                        fill: 'rgba(0,0,0,0.3)',
+                        draggable: true,
+                    })
+                    //@ts-ignore
+                    ball_layer.value?.getNode().add(circle);
                 }
-            };
-        }, circleNode.getLayer());
-        anim.start();
-    }
+            }, circle2node.getLayer());
+            anim.start();
+        }
+        // if(circle.value){
+        //     bounceball(circle.value, stage_cfg, circle_cfg)
+        // }
+    })
+
+    // function bounceball(
+    //     shape: Konva.Circle, 
+    //     stage_cfg: StageConfig,
+    //     shape_cfg: CircleConfig,
+    // ){
+    //     // @ts-ignore
+    //     const circleNode = shape.getNode()
+    //     let positionX = shape_cfg.x                    
+    //     let positionY = shape_cfg.y
+
+    //     const period: number = 3;
+    //     let velX = 10 / period;
+    //     let velY = velX;
+
+    //     let anim = new Konva.Animation(function(frame) {
+    //         if(frame){
+    //             let borderXl = positionX - shape_cfg.radius + velX;
+    //             let borderXr = positionX + shape_cfg.radius + velX;
+    //             if(borderXl > 0 && borderXr < stage_cfg.width){
+    //             } else if(borderXl <= 0){
+    //                 velX = frame.timeDiff / period;
+    //             } else if (borderXr >= stage_cfg.width) {
+    //                 velX = -frame.timeDiff / period;
+    //             };
+                
+    //             let borderYt = positionY - shape_cfg.radius + velY;
+    //             let borderYb = positionY + shape_cfg.radius + velY;
+    //             if(borderYt > 0 && borderYb < stage_cfg.height){
+    //             } else if(borderYt <= 0){
+    //                 velY = frame.timeDiff / period;
+    //             } else if (borderYb >= stage_cfg.height) {
+    //                 velY = -frame.timeDiff / period;
+    //             };
+
+    //             positionX += velX;
+    //             positionY += velY;
+    //             circleNode.setX(positionX);
+    //             circleNode.setY(positionY);
+
+    //             if (positionX && positionY){
+    //                 posX.value = Math.round(positionX);
+    //                 posY.value = Math.round(positionY);
+    //             }
+    //         };
+    //     }, circleNode.getLayer());
+    //     anim.start();
+    // }
     const width = window.innerWidth;
     const height = window.innerHeight;
     
@@ -120,9 +147,18 @@
         strokeWidth: number,
         draggable: boolean,
     }
-    const circle_cfg: CircleConfig = {
-        x: 100,
-        y: 500,
+    // const circle_cfg: CircleConfig = {
+    //     x: 100,
+    //     y: 500,
+    //     radius: 30,
+    //     fill: "rgba(100,200,120,0.2)",
+    //     stroke: "black",
+    //     strokeWidth: 1,
+    //     draggable: true,
+    // }
+    const circle_cfg2: CircleConfig = {
+        x: 50,
+        y: 50,
         radius: 30,
         fill: "rgba(100,200,120,0.2)",
         stroke: "black",
