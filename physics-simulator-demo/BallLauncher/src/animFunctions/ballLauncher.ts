1ec9eafbb081df9999a77358af4dc347f756dd53
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-02 15:15:36 +0900

    変数名を修正

--- physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
+++ physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
@@ -1,5 +1,5 @@
-import Konva from "konva";
-import { CircleConfig } from '@/configs/circleConfig'
+import Konva from 'konva';
+import { CircleConfig } from '@/configs/circleConfig';
 
 export function ballLauncher(
     shape: Konva.Circle | undefined,
@@ -11,17 +11,17 @@ export function ballLauncher(
                 
     let anim = new Konva.Animation(function(frame){
         if(frame) {
-            let aaa = frame.time / period;
+            let time = frame.time / period;
             let velX = 50;
-            shapenode.setX(circle_cfg.x + velX * aaa)
-            shapenode.setY(circle_cfg.y +  9.8 * aaa ** 2)
+            shapenode.setX(circle_cfg.x + velX * time);
+            shapenode.setY(circle_cfg.y +  9.8 * time ** 2)
             let circle = new Konva.Circle({
-                x: circle_cfg.x + velX * aaa,
-                y: circle_cfg.y +  9.8 * aaa ** 2,
+                x: circle_cfg.x + velX * time,
+                y: circle_cfg.y +  9.8 * time ** 2,
                 radius: 5,
                 fill: 'rgba(210,110,100,0.5)',
                 draggable: true,
-            })
+            });
             //@ts-ignore
             shapenode.getLayer().add(circle);
         }
