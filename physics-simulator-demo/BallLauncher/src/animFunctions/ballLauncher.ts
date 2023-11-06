79420ab50969ad4ee4e8d9615795a90d06d385c4
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-06 11:56:57 +0900

    アニメーションが画面外でも継続していた問題を修正

--- physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
+++ physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
@@ -1,5 +1,6 @@
 import Konva from 'konva';
 import { CircleConfig } from '@/configs/circleConfig';
+import { stage_cfg } from '@/configs/stageConfig';
 
 export function ballLauncher(
     shape: Konva.Circle | undefined,
@@ -9,14 +10,13 @@ export function ballLauncher(
 ){
     //@ts-ignore
     const shapenode = shape.getNode();
-    let period = 1000 / speed ;
                 
     let anim = new Konva.Animation(function(frame){
         if(frame) {
-            let time = frame.time / period;
+            let time = frame.time * speed * 0.005;
             let velX = velocity * 10;
             const shapePositionX = circle_cfg.x + velX * time;
-            const shapePositionY = circle_cfg.y + 0.5 * 9.8 * time ** 2;
+            const shapePositionY = circle_cfg.y + 0.2 * 9.8 * time ** 2;
             shapenode.setX(shapePositionX);
             shapenode.setY(shapePositionY);
             let circle = new Konva.Circle({
@@ -28,6 +28,9 @@ export function ballLauncher(
             });
             //@ts-ignore
             shapenode.getLayer().add(circle);
+            if(shapenode.y() >= stage_cfg.height + circle_cfg.radius){
+                anim.stop()
+            }
         }
     }, shapenode.getLayer());
     anim.start();
