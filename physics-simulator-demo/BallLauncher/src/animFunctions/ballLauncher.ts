d0f233f6c68ec3a1f534ddb813af74e93f1b1189
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-09 13:30:40 +0900

    投射物の軌跡を表示する関数を追加

--- physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
+++ physics-simulator-demo/BallLauncher/src/animFunctions/ballLauncher.ts
@@ -1,44 +1,56 @@
 import Konva from 'konva';
 import { CircleConfig } from '@/configs/circleConfig';
 import { stage_cfg } from '@/configs/stageConfig';
+// import { GraphTrack } from '@/configs/track';
 
 export function ballLauncher(
     shape: Konva.Circle | undefined,
     circle_cfg: CircleConfig,
     speed: number,
     velocity: number,
+    interval: number,
+    // configs: GraphTrack[]
 ){
     //@ts-ignore
     const shapenode = shape.getNode();
-    let velX = velocity * 10;
                 
     let anim = new Konva.Animation(function(frame){
         if(frame) {
             let time = frame.time * speed * 0.005;
-            let lastTime = (frame.time - frame.timeDiff) * speed * 0.005;
             const positionX = (time:number): number => {
-                return circle_cfg.x + velX * time;
+                return circle_cfg.x + velocity * interval * time;
             };
             const positionY = (time:number): number => {
-                return circle_cfg.y + 0.2 * 9.8 * time ** 2
+                return circle_cfg.y + 0.5 * 9.8 * interval * time ** 2
             };
             shapenode.setX(positionX(time));
             shapenode.setY(positionY(time));
-            let line = new Konva.Line({
-                points: [
-                    positionX(lastTime), positionY(lastTime),
-                    positionX(time), positionY(time)
-                ],
-                stroke: `${circle_cfg.stroke}`,
-                strokeWidth: 3,
-            });
-            //@ts-ignore
-            shapenode.getLayer().add(line);
-
-            if(shapenode.y() >= stage_cfg.height + circle_cfg.radius){
-                anim.stop()
-            }
-        }
+        };
+        if(shapenode.y() >= stage_cfg.height + circle_cfg.radius){
+            anim.stop();
+        };
     }, shapenode.getLayer());
     anim.start();
-}
+    // configs.push(ballTrack(circle_cfg, velocity, interval));
+};
+export function ballTrack(
+    circle_cfg: CircleConfig,
+    velocity: number,
+    interval: number,
+) {
+    const point = [];
+    for(let i=0; i <= 20; i+=0.1){
+        point.push(
+            circle_cfg.x + velocity * interval * i, 
+            circle_cfg.y + 0.5 * 9.8 * interval * i ** 2
+        )
+    };
+    const line = {
+        points: point,
+        stroke: `${circle_cfg.stroke}`,
+        strokeWidth: 3,
+        config: circle_cfg,
+        velocity: velocity,
+    };
+    return line;
+};
