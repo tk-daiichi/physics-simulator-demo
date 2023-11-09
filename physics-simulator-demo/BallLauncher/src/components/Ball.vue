9a844facc3078bec886dd8ef0d7df9b6631fa829
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-09 13:31:43 +0900

    軌跡を表示するテンプレートを追加

--- physics-simulator-demo/BallLauncher/src/components/Ball.vue
+++ physics-simulator-demo/BallLauncher/src/components/Ball.vue
@@ -1,17 +1,17 @@
 <template>
     <v-layer>
         <v-circle :config="ba1Cfg" ref="ba1"/>
-        <v-circle :config="ba2Cfg" ref="ba2"/>
+        <v-line :config="config" v-for="config in configs"></v-line>
     </v-layer>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { ref, reactive } from 'vue';
     import Konva from 'konva';
     import { origin } from '@/configs/origin'
+    import { GraphTrack } from '@/configs/track';
 
     const ba1 = ref<Konva.Circle>();
-    const ba2 = ref<Konva.Circle>();
     const ba1Cfg = {
         x: origin.x,
         y: origin.y,
@@ -20,20 +20,12 @@
         stroke: "rgba(200,0,0,1)",
         strokeWidth: 1,
         draggable: true,
-    }
-    const ba2Cfg = {
-        x: origin.x,
-        y: origin.y,
-        radius: 15,
-        // fill: "rgba(0,0,255,1)",
-        stroke: "black",
-        strokeWidth: 1,
-        draggable: true,
-    }
+    };
+    const configs = reactive<GraphTrack[]>([])
+
     defineExpose({
         ba1,
-        ba2,
         ba1Cfg,
-        ba2Cfg,
+        configs,
     });
 </script>
