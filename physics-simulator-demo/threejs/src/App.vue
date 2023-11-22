d39918de3912fed0091ce2c1d21542ddcc4b9878
tk-daiichi <134348859+tk-daiichi@users.noreply.github.com>
2023-11-22 10:44:40 +0900

    コンポーネント名を変更

--- physics-simulator-demo/threejs/src/App.vue
+++ physics-simulator-demo/threejs/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <Threejs/>
+  <!-- <Test3D/> -->
+  <WaveSim/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Threejs from '@/components/Threejs.vue'
+import Test3D from '@/components/Test3D.vue'
+import WaveSim from '@/components/WaveSim.vue'
 
 export default defineComponent({
   name: "App",
   components: {
-    Threejs,
+    Test3D,
+    WaveSim,
   },
 });
 </script>
